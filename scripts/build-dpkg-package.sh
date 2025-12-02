f020a104b8cef4aea5ccee72a712a5f1e4147aea
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2025-12-02 10:56:17 +0100

    fix release workflow

--- scripts/build-dpkg-package.sh
+++ scripts/build-dpkg-package.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ -z $1 ]]; then
-    echo "usage: $(basename "$0") [arch]"
+    echo "usage: $(basename "$0") {arch} [output-file-name]"
     exit 1
 fi
 
@@ -14,4 +14,4 @@ sed 's/<<VERSION>>/'"${tag:1}"'/' -i dist/dpkg/DEBIAN/control
 sed 's/<<ARCH>>/'"${arch}"'/' -i dist/dpkg/DEBIAN/control
 
 dpkg-deb -Zxz --build --root-owner-group \
-    dist/dpkg "echo-${arch}.deb"
+    dist/dpkg "${2:-"echo-${arch}.deb"}"
